ca4aca849bc46e8ab04dafbc9741ac8d13a860bb
rabbaniST <golamrabbanig5@gmail.com>
2025-03-28 19:50:37 +0600

    migration files add for databases

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
             $table->string('email')->unique();
+            $table->string('mobile', 20)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('otp', 10);
             $table->rememberToken();
             $table->timestamps();
         });
